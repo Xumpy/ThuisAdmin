e49154ee688c24dd981fe2bc7de8755522f2a62e
Nico Martens <nico@MacBook-Pro.localdomain>
2024-06-16 17:29:14 +0200

    Updated Overview PDF Builder to work with pages

--- src/main/java/com/xumpy/finances/services/OverviewPDFBuilder.java
+++ src/main/java/com/xumpy/finances/services/OverviewPDFBuilder.java
@@ -5,18 +5,18 @@ import com.itextpdf.text.pdf.*;
 import com.xumpy.thuisadmin.controllers.model.OverzichtGroepBedragen;
 import com.xumpy.thuisadmin.dao.implementations.DocumentenDaoImpl;
 import com.xumpy.thuisadmin.dao.model.DocumentenDaoPojo;
+import com.xumpy.thuisadmin.domain.Documenten;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.text.ParseException;
-import java.util.Base64;
-import java.util.HashMap;
+import java.util.*;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class OverviewPDFBuilder {
@@ -30,32 +30,40 @@ public class OverviewPDFBuilder {
         return tableHeader;
     }
 
-    private BigDecimal buildTotal(List<OverzichtGroepBedragen> overzichtGroepBedragen){
+    private BigDecimal buildTotal(Map<OverzichtGroepBedragen, Integer> overzichtGroepBedragen){
         BigDecimal sum = new BigDecimal(0);
-        for (OverzichtGroepBedragen overzichtGroepBedrag: overzichtGroepBedragen){
-            sum = sum.add(overzichtGroepBedrag.getBedrag());
+        for (Map.Entry<OverzichtGroepBedragen, Integer> overzichtGroepBedrag: overzichtGroepBedragen.entrySet()){
+            sum = sum.add(overzichtGroepBedrag.getKey().getBedrag());
         }
         return sum;
     }
 
-    private PdfPTable createOverviewExportedAmountsTable(List<OverzichtGroepBedragen> overzichtGroepBedragen) throws ParseException {
-        PdfPTable table = new PdfPTable(4);
+    private String pageNumber(Integer sizeHeader, Integer currentPage) {
+        if (sizeHeader == null) {
+            return "Yes";
+        } else {
+            return Integer.toString(sizeHeader + currentPage);
+        }
+    }
+
+    private PdfPTable createOverviewExportedAmountsTable(Map<OverzichtGroepBedragen, Integer> overzichtGroepBedragen, Integer sizeHeader) throws ParseException {
+        PdfPTable table = new PdfPTable(new float[] { 20, 15, 50, 15 });
 
         PdfPCell dateColumn = new PdfPCell(createTableHeader("Date"));
         PdfPCell amountColumn = new PdfPCell(createTableHeader("Amount"));
         PdfPCell descriptionColumn = new PdfPCell(createTableHeader("Description"));
-        PdfPCell availableColumn = new PdfPCell(createTableHeader("Document Available"));
+        PdfPCell availableColumn = new PdfPCell(createTableHeader("Page"));
 
         table.addCell(dateColumn);
         table.addCell(amountColumn);
         table.addCell(descriptionColumn);
         table.addCell(availableColumn);
 
-        for(OverzichtGroepBedragen overzichtGroepBedrag: overzichtGroepBedragen){
-            PdfPCell datum = new PdfPCell(new Paragraph(overzichtGroepBedrag.getDatum()));
-            PdfPCell amount = new PdfPCell(new Paragraph(overzichtGroepBedrag.getBedrag().setScale(2, RoundingMode.HALF_UP).toString()));
-            PdfPCell description = new PdfPCell(new Paragraph(overzichtGroepBedrag.getOmschrijving()));
-            PdfPCell documentAvailable = new PdfPCell(new Paragraph(mappedDocuments.get(overzichtGroepBedrag.getPk_id()).isEmpty() ? "No": "Yes"));
+        for(Map.Entry<OverzichtGroepBedragen, Integer> overzichtGroepBedrag: overzichtGroepBedragen.entrySet()){
+            PdfPCell datum = new PdfPCell(new Paragraph(overzichtGroepBedrag.getKey().getDatum()));
+            PdfPCell amount = new PdfPCell(new Paragraph(overzichtGroepBedrag.getKey().getBedrag().setScale(2, RoundingMode.HALF_UP).toString()));
+            PdfPCell description = new PdfPCell(new Paragraph(overzichtGroepBedrag.getKey().getOmschrijving()));
+            PdfPCell documentAvailable = new PdfPCell(new Paragraph(mappedDocuments.get(overzichtGroepBedrag.getKey().getPk_id()).isEmpty() ? "": pageNumber(sizeHeader, overzichtGroepBedrag.getValue())));
 
             table.addCell(datum);
             table.addCell(amount);
@@ -65,29 +73,103 @@ public class OverviewPDFBuilder {
         return  table;
     }
 
-    private void attachPdfs(List<OverzichtGroepBedragen> overzichtGroepBedragen, PdfCopy copy) throws IOException, DocumentException {
-        for(OverzichtGroepBedragen overzichtGroepBedrag: overzichtGroepBedragen){
-            for(DocumentenDaoPojo documenten: mappedDocuments.get(overzichtGroepBedrag.getPk_id())){
-                PdfReader reader = new PdfReader(documenten.getDocument());
-                copy.addDocument(reader);
-                reader.close();
-            }
-        }
-    }
-
-    private void createPDF(PdfCopy copy, List<OverzichtGroepBedragen> overzichtGroepBedragen) throws DocumentException, IOException, ParseException {
+    private Map.Entry<byte[], Map<OverzichtGroepBedragen, Integer>> generatePdfs(List<OverzichtGroepBedragen> overzichtGroepBedragen) throws IOException, DocumentException {
         Document document = new Document(PageSize.A4);
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         PdfWriter.getInstance(document, byteArrayOutputStream);
+        PdfCopy copy = new PdfSmartCopy(document, byteArrayOutputStream);
+
+        document.open();
+        copy.open();
+
+        Map<OverzichtGroepBedragen, Integer> documentPerPage = new LinkedHashMap<>();
+
+        for(OverzichtGroepBedragen overzichtGroepBedrag: overzichtGroepBedragen){
+            documentPerPage.put(overzichtGroepBedrag, copy.getPageNumber());
+
+            for(DocumentenDaoPojo documenten: mappedDocuments.get(overzichtGroepBedrag.getPk_id())){
+                try {
+                    PdfReader reader = new PdfReader(documenten.getDocument());
+                    copy.addDocument(reader);
+                    reader.close();
+                } catch (Exception exception){
+                    System.out.println("Exception with document: " + documenten.getOmschrijving() + " - " + documenten.getPk_id() + " : Bedrag: " + documenten.getBedrag().getOmschrijving());
+                }
+            }
+        }
+        copy.close();
+
+        return new Map.Entry<>() {
+            @Override
+            public byte[] getKey() {
+                return byteArrayOutputStream.toByteArray() ;
+            }
+
+            @Override
+            public Map<OverzichtGroepBedragen, Integer> getValue() {
+                return documentPerPage;
+            }
+
+            @Override
+            public Map<OverzichtGroepBedragen, Integer> setValue(Map<OverzichtGroepBedragen, Integer> value) {
+                throw new RuntimeException("Not implemented");
+            }
+        };
+    }
+
+    private Integer getHeaderPageSize(Map<OverzichtGroepBedragen, Integer> overzichtGroepBedragen) throws DocumentException, ParseException, IOException {
+        Document document = new Document(PageSize.A4);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PdfWriter.getInstance(document, byteArrayOutputStream);
+
         document.open();
 
         document.add(createTableHeader("Overview"));
-        document.add(createOverviewExportedAmountsTable(overzichtGroepBedragen));
+        document.add(createOverviewExportedAmountsTable(overzichtGroepBedragen, null));
         document.add(createTableHeader("Total: " + buildTotal(overzichtGroepBedragen)));
+
         document.close();
 
-        copy.addDocument(new PdfReader(byteArrayOutputStream.toByteArray()));
-        attachPdfs(overzichtGroepBedragen, copy);
+        PdfReader pdfReader = new PdfReader(byteArrayOutputStream.toByteArray());
+
+        return pdfReader.getNumberOfPages();
+    }
+
+    private byte[] createHeader(Map<OverzichtGroepBedragen, Integer> overzichtGroepBedragen) throws IOException, DocumentException, ParseException {
+        Document document = new Document(PageSize.A4);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PdfWriter.getInstance(document, byteArrayOutputStream);
+
+        document.open();
+
+        document.add(createTableHeader("Overview"));
+        document.add(createOverviewExportedAmountsTable(overzichtGroepBedragen, getHeaderPageSize(overzichtGroepBedragen)));
+        document.add(createTableHeader("Total: " + buildTotal(overzichtGroepBedragen)));
+
+        document.close();
+
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    private ByteArrayOutputStream createPDF(List<OverzichtGroepBedragen> overzichtGroepBedragen) throws DocumentException, IOException, ParseException {
+        Document document = new Document(PageSize.A4);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PdfCopy copy = new PdfSmartCopy(document, byteArrayOutputStream);
+        document.open();
+
+        Map.Entry<byte[], Map<OverzichtGroepBedragen, Integer>> overzichtBedragen = generatePdfs(overzichtGroepBedragen);
+
+        PdfReader header = new PdfReader(createHeader(overzichtBedragen.getValue()));
+        copy.addDocument(header);
+        header.close();
+
+        PdfReader pdfs = new PdfReader(overzichtBedragen.getKey());
+        copy.addDocument(pdfs);
+        pdfs.close();
+
+        copy.close();
+
+        return byteArrayOutputStream;
     }
 
     private void buildMappedDocuments(List<OverzichtGroepBedragen> overzichtGroepBedragen){
@@ -100,16 +182,7 @@ public class OverviewPDFBuilder {
     public String buildPDFOveriew(List<OverzichtGroepBedragen> overzichtGroepBedragen) throws DocumentException, IOException, ParseException {
         buildMappedDocuments(overzichtGroepBedragen);
 
-        ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
-
-        Document document = new Document(PageSize.A4);
-        PdfCopy copy = new PdfSmartCopy(document, byteStream);
-
-        document.open();
-        createPDF(copy, overzichtGroepBedragen);
-        document.close();
-
-        byte[] encoded = Base64.getEncoder().encode(byteStream.toByteArray());
+        byte[] encoded = Base64.getEncoder().encode(createPDF(overzichtGroepBedragen).toByteArray());
         return new String(encoded);
     }
 }
